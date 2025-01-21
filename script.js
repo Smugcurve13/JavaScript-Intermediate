22a96145bcb4d3e2278b3c91948f0250687004c8
SambhavSoni-Compunnel <sambhav.soni@compunneldigital.com>
2025-01-21 11:38:05 +0530

    objects

--- script.js
+++ script.js
@@ -17,10 +17,30 @@
 // // Lenghth of an Array
 // console.log(fruits.length); // Number of Elements
 
-const languages = ['python','js','c++']
-console.log(languages[0])
-console.log(languages[2])
-languages.push("html")
-console.log(languages)
-languages.pop()
-console.log(languages)
+// const languages = ['python','js','c++']
+// console.log(languages[0])
+// console.log(languages[2])
+// languages.push("html")
+// console.log(languages)
+// languages.pop()
+// console.log(languages)
+
+// OBJECT CREATION
+const person = {
+    name: "John",
+    age: 25,
+    isStudent: true
+};
+console.log(person)
+
+// Accessing Properties
+console.log(person.name);
+console.log(person["age"]);
+
+// Updating Properties
+person.age = 26;
+console.log(person.age);
+
+// Adding New Properties
+person.country = "India"
+console.log(person);
