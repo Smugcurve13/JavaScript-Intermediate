f1ebd53c8117a19cef8aaed1959cb63ae5507888
SambhavSoni-Compunnel <sambhav.soni@compunneldigital.com>
2025-01-21 11:22:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Array creation
+const fruits = ['apple','banana','cherry'];
+console.log(fruits)
+
+// Accessing Elements
+console.log(fruits[0])
+console.log(fruits[2])
+
+// Adding Elements
+fruits.push("orange");
+console.log(fruits);
+
+// Removing Elements
+fruits.pop();  // Removes the last element
+console.log(fruits);
+
+// Lenghth of an Array
+console.log(fruits.length); // Number of Elements
