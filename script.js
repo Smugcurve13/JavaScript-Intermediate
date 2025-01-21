630143e16a4fbc888f3649a751a9e6c19712e157
SambhavSoni-Compunnel <sambhav.soni@compunneldigital.com>
2025-01-21 11:32:40 +0530

    practising with arrays

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-// Array creation
-const fruits = ['apple','banana','cherry'];
-console.log(fruits)
+// // Array creation
+// const fruits = ['apple','banana','cherry'];
+// console.log(fruits)
 
-// Accessing Elements
-console.log(fruits[0])
-console.log(fruits[2])
+// // Accessing Elements
+// console.log(fruits[0])
+// console.log(fruits[2])
 
-// Adding Elements
-fruits.push("orange");
-console.log(fruits);
+// // Adding Elements
+// fruits.push("orange");
+// console.log(fruits);
 
-// Removing Elements
-fruits.pop();  // Removes the last element
-console.log(fruits);
+// // Removing Elements
+// fruits.pop();  // Removes the last element
+// console.log(fruits);
 
-// Lenghth of an Array
-console.log(fruits.length); // Number of Elements
+// // Lenghth of an Array
+// console.log(fruits.length); // Number of Elements
+
+const languages = ['python','js','c++']
+console.log(languages[0])
+console.log(languages[2])
+languages.push("html")
+console.log(languages)
+languages.pop()
+console.log(languages)
